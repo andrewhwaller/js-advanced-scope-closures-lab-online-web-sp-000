27c5cb2d4b14dfb89d5051a4e09f747606bec62e
Andrew Waller <andrewhwaller@gmail.com>
2019-09-12 02:12:08 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function produceDrivingRange(blockRange) {
+  return function (startBlock, endBlock) {
+
+    let start = parseInt(startBlock);
+    let end = parseInt(endBlock);
+    let distanceToTravel = Math.abs( end - start );
+    let difference = blockRange - distanceToTravel;
+
+    if ( difference > 0 ) {
+      return `within range by ${difference}`
+    } else {
+      return `${Math.abs(difference)} blocks out of range`
+    }
+  }
+}
